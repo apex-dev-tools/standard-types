78e66838506ba9db2f96ced6515259d0e54ea218
Peter Wright <pwright@financialforce.com>
2022-12-07 10:40:29 +0000

    feat: Add more methods to ConnectApi.Wave

--- src/main/java/com/nawforce/runforce/ConnectApi/Wave.java
+++ src/main/java/com/nawforce/runforce/ConnectApi/Wave.java
@@ -7,4 +7,7 @@ package com.nawforce.runforce.ConnectApi;
 @SuppressWarnings("unused")
 public class Wave {
     public static LiteralJson executeQuery(String queryString) {throw new java.lang.UnsupportedOperationException();}
+    public static LiteralJson executeQuery(String queryString, String timezone) {throw new java.lang.UnsupportedOperationException();}
+    public static LiteralJson executeQueryByName(String name) {throw new java.lang.UnsupportedOperationException();}
+    public static LiteralJson executeQueryByName(String name, String timezone) {throw new java.lang.UnsupportedOperationException();}
 }
