4d367d7ca199cbb426393c2cebd2366d42c768ac
Peter Wright <pwright@financialforce.com>
2022-12-08 18:53:25 +0000

    fix: Add Invocable.Action Error and Result inner classes

--- src/main/java/com/nawforce/runforce/Invocable/Action.java
+++ src/main/java/com/nawforce/runforce/Invocable/Action.java
@@ -11,6 +11,21 @@ import com.nawforce.runforce.System.String;
 
 @SuppressWarnings("unused")
 public class Action {
+    public static class Error {
+        public Object clone() {throw new java.lang.UnsupportedOperationException();}
+        public String getCode() {throw new java.lang.UnsupportedOperationException();}
+        public String getMessage() {throw new java.lang.UnsupportedOperationException();}
+    }
+
+    public static class Result {
+        public Object clone() {throw new java.lang.UnsupportedOperationException();}
+        public Action getAction() {throw new java.lang.UnsupportedOperationException();}
+        public List<Action.Error> getErrors() {throw new java.lang.UnsupportedOperationException();}
+        public Map<String,Object> getInvocationParameters() {throw new java.lang.UnsupportedOperationException();}
+        public Map<String,Object> getOutputParameters() {throw new java.lang.UnsupportedOperationException();}
+        public Boolean isSuccess() {throw new java.lang.UnsupportedOperationException();}
+    }
+
     public Action addInvocation() {throw new java.lang.UnsupportedOperationException();}
     public Action clearInvocations() {throw new java.lang.UnsupportedOperationException();}
     public Object clone() {throw new java.lang.UnsupportedOperationException();}
@@ -20,7 +35,7 @@ public class Action {
     public String getName() {throw new java.lang.UnsupportedOperationException();}
     public String getNamespace() {throw new java.lang.UnsupportedOperationException();}
     public String getType() {throw new java.lang.UnsupportedOperationException();}
-    public List<Result> invoke() {throw new java.lang.UnsupportedOperationException();}
+    public List<Action.Result> invoke() {throw new java.lang.UnsupportedOperationException();}
     public Boolean isStandard() {throw new java.lang.UnsupportedOperationException();}
     public Action setInvocationParameter(String parameterName, Object parameterValue) {throw new java.lang.UnsupportedOperationException();}
     public Action setInvocations(List<Map<String,Object>> invocations) {throw new java.lang.UnsupportedOperationException();}
