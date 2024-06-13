1f68934bf799deabd13a0b4470578d1e73e22100
Peter Wright <pwright@certinia.com>
2024-06-13 14:25:04 +0100

    feat: add Context namespace

---
+++ src/main/java/com/nawforce/runforce/Context/IndustriesContext.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 Certinia Inc. All rights reserved.
+ */
+
+package com.nawforce.runforce.Context;
+
+import com.nawforce.runforce.System.Map;
+
+@SuppressWarnings("unused")
+public class IndustriesContext {
+
+  public IndustriesContext() {throw new java.lang.UnsupportedOperationException();}
+
+  public Map<String,Object> buildContext(Map<String,Object> input) {throw new java.lang.UnsupportedOperationException();}
+  public void deleteContext(Map<String,Object> input) {throw new java.lang.UnsupportedOperationException();}
+  public Map<String,Object> getContext(Map<String,Object> input) {throw new java.lang.UnsupportedOperationException();}
+  public Map<String,Object> getContextTranslation(Map<String,Object> input) {throw new java.lang.UnsupportedOperationException();}
+  public Map<String,Object> persistContext(Map<String,Object> input) {throw new java.lang.UnsupportedOperationException();}
+  public Map<String,Object> queryRecordStatus(Map<String,Object> input) {throw new java.lang.UnsupportedOperationException();}
+  public Map<String,Object> queryTags(Map<String,Object> input) {throw new java.lang.UnsupportedOperationException();}
+  public Map<String,Object> updateContextAttributes(Map<String,Object> input) {throw new java.lang.UnsupportedOperationException();}
+}
