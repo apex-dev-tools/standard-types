8c09b841d6f62a8a418559206d18bc587d51e3bc
Peter Wright <pwright@certinia.com>
2024-11-01 17:08:43 +0000

    feat: add new api 62 types for sfdx_enablement namespace

---
+++ src/main/java/com/nawforce/runforce/sfdc_enablement/LearningEvaluationResult.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 Certinia Inc. All rights reserved.
+ */
+
+package com.nawforce.runforce.sfdc_enablement;
+
+import com.nawforce.runforce.System.Double;
+
+@SuppressWarnings("unused")
+public class LearningEvaluationResult {
+  public LearningEvaluationResult() {throw new java.lang.UnsupportedOperationException();}
+
+  public Double getLearningItemProgress() {throw new UnsupportedOperationException();}
+  public LearningItemProgressStatus getLearningItemProgressStatus() {throw new UnsupportedOperationException();}
+  public void setLearningItemProgress(Double learningItemProgress) {throw new UnsupportedOperationException();}
+  public void setLearningItemProgressStatus(LearningItemProgressStatus learningItemProgressStatus) {throw new UnsupportedOperationException();}
+}
