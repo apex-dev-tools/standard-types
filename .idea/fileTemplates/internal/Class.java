4ccedb55b1d31488864263c649c10756147d6628
Peter Wright <pwright@certinia.com>
2024-11-01 14:25:15 +0000

    chore: add intellij file templates

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) ${YEAR} Certinia Inc. All rights reserved.
+ */
+
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+@SuppressWarnings("unused")
+public class ${NAME} {
+
+//  public ${NAME}() {throw new java.lang.UnsupportedOperationException();}
+
+}
---
+++ .idea/fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) ${YEAR} Certinia Inc. All rights reserved.
+ */
+
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+@SuppressWarnings("unused")
+public interface ${NAME} {
+}
