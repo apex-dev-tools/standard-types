832bdfc7a8d91938d7b30c3d2bb4b20b986d4a99
Peter Wright <pwright@certinia.com>
2024-11-01 17:08:09 +0000

    feat: add new api 62 types for industriesNlpSvc namespace

---
+++ src/main/java/com/nawforce/runforce/industriesNlpSvc/NlpResponse.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Certinia Inc. All rights reserved.
+ */
+
+package com.nawforce.runforce.industriesNlpSvc;
+
+import com.nawforce.runforce.System.List;
+import com.nawforce.runforce.System.String;
+
+@SuppressWarnings("unused")
+public class NlpResponse {
+  public NlpSummarizationResult summarizationResult;
+  public List<String> errors;
+
+  public NlpResponse() {throw new java.lang.UnsupportedOperationException();}
+}
---
+++ src/main/java/com/nawforce/runforce/industriesNlpSvc/NlpSummarizationResult.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Certinia Inc. All rights reserved.
+ */
+
+package com.nawforce.runforce.industriesNlpSvc;
+
+import com.nawforce.runforce.System.String;
+
+@SuppressWarnings("unused")
+public class NlpSummarizationResult {
+  public String summary;
+
+  public NlpSummarizationResult() {throw new java.lang.UnsupportedOperationException();}
+}
