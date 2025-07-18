2b0a6cdb9c45079c13cecf35852d396c759e5d3d
Peter Wright <pwright@certinia.com>
2025-07-18 14:38:28 +0100

    feat: add CommerceBuyGrp namespace 64

---
+++ src/main/java/com/nawforce/runforce/CommerceBuyGrp/BuyerGroupEvaluationService.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Certinia Inc. All rights reserved.
+ */
+
+package com.nawforce.runforce.CommerceBuyGrp;
+
+@SuppressWarnings("unused")
+public class BuyerGroupEvaluationService {
+  public BuyerGroupEvaluationService() {throw new java.lang.UnsupportedOperationException();}
+
+  public BuyerGroupResponse getBuyerGroupIds(BuyerGroupRequest request) {throw new java.lang.UnsupportedOperationException();}
+}
---
+++ src/main/java/com/nawforce/runforce/CommerceBuyGrp/BuyerGroupRequest.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 Certinia Inc. All rights reserved.
+ */
+
+package com.nawforce.runforce.CommerceBuyGrp;
+
+import com.nawforce.runforce.System.Map;
+import com.nawforce.runforce.System.String;
+
+@SuppressWarnings("unused")
+public class BuyerGroupRequest {
+  public BuyerGroupRequest() {throw new java.lang.UnsupportedOperationException();}
+
+  public String getAccountId() {throw new java.lang.UnsupportedOperationException();}
+  public String getStoreId() {throw new java.lang.UnsupportedOperationException();}
+  public Map<String,Object> getRequestContextParameters() {throw new java.lang.UnsupportedOperationException();}
+}
